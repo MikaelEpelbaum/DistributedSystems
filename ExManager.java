ec6ddab455029c5b91fd0d23796c6d9b8e81a361
mikael epelbaum <mikaelepelbaum@gmail.com>
2023-01-06 13:13:45 +0200

    Link state routing implemented but synchronisations problem. need to create event driven code

--- ExManager.java
+++ ExManager.java
@@ -12,10 +12,9 @@ public class ExManager {
         initial_data = new Scanner(new File(path));
     }
 
-//  todo: which one of the functions to keep? weight for response
-    public Node getNode(int id){
-        return network[id];
-    }
+//  todo: which one of the functions to keep? wait for response
+
+//    public Node getNode(int id){return network[id];}
     public Node get_node(int i) {
         return network[i];
     }
@@ -25,24 +24,39 @@ public class ExManager {
     }
 
     public void update_edge(int id1, int id2, double weight){
-        network[id1].edge_update(id2, weight);
-        network[id2].edge_update(id1, weight);
+        network[id1-1].edge_update(id2, weight);
+        network[id2-1].edge_update(id1, weight);
     }
 
     public void read_txt() {
         num_of_nodes = Integer.parseInt(initial_data.nextLine());
-        this.network = new Node[num_of_nodes+1];
+        this.network = new Node[num_of_nodes];
         String line;
         while (initial_data.hasNextLine()) {
              line = initial_data.nextLine();
             if (line.equals("stop"))
                 break;
             Node n = new Node(num_of_nodes, line);
-            network[n.get_id()] = n;
+            network[n.get_id()-1] = n;
         }
     }
 
     public void start(){
-        // your code here
+//        Map<Integer, Boolean> updated = new HashMap<>();
+//        for (int i = 0; i < num_of_nodes; i++)
+//            updated.put(i, false);
+
+        for (int i = 0; i < num_of_nodes; i++)
+            network[i].run();
+
+        System.out.println("");
+//        try {
+//            if (updated.containsValue(false)) {
+//                throw new Exception();
+//            }
+//        }
+//        catch (Exception e){
+//            System.out.println("problem");
+//        }
     }
 }
--- Node.java
+++ Node.java
@@ -1,4 +1,8 @@
+import java.sql.Struct;
 import java.util.*;
+import java.net.*;
+import java.util.*;
+import java.io.*;
 
 public class Node extends Thread {
     private int id;
@@ -15,9 +19,9 @@ public class Node extends Thread {
      */
     public Node(int num_of_nodes, String line){
         this.num_of_nodes = num_of_nodes;
-        matrix = new double[num_of_nodes + 1][num_of_nodes + 1];
-        for(int i = 0; i < num_of_nodes+1; i++){
-            for (int j = 0; j < num_of_nodes+1; j++){
+        matrix = new double[num_of_nodes][num_of_nodes];
+        for(int i = 0; i < num_of_nodes; i++){
+            for (int j = 0; j < num_of_nodes; j++){
                 matrix[i][j] = -1;
             }
         }
@@ -58,10 +62,9 @@ public class Node extends Thread {
      * @param edge
      * updates neighbor edge values in neighbors matrix
      */
-//    todo: arrange matrix indexes from 1 instead of 0
     public void update_matrix(Pair<Integer, Map> edge){
         Map<String, Double> temp = edge.getValue();
-        this.matrix[this.id][edge.getKey()] = temp.get("weight");
+        this.matrix[this.id-1][edge.getKey()-1] = temp.get("weight");
     }
 
     public void edge_update(int id, double weight){
@@ -75,11 +78,72 @@ public class Node extends Thread {
         }
     }
 
-    public void LinkStateRouting(){
-//        send weights of v to all graph
-        double[] lv = matrix[id];
+    @Override
+    public void run(){
+        Pair<Integer, double[]> lv = new Pair<>(id, matrix[id-1]);
+
+        Map<Integer, Boolean> updated = new HashMap<>();
+        for (int i = 0; i < num_of_nodes; i++)
+            updated.put(i, false);
+        updated.put(id-1, true);
 
 
+//      sends my lv to all my neighbors
+        for (int i = 0; i < neighbors.size(); i++) {
+            Map<String, Double> neighbor =  (Map<String, Double>)neighbors.get(i).getValue();
+            send_data(neighbor, lv);
+        }
+
+        while (updated.containsValue(false)) {
+            for (int i = 0; i < neighbors.size(); i++) {
+                Map<String, Double> neighbor =  (Map<String, Double>)neighbors.get(i).getValue();
+                int port_origin = neighbor.get("port_origin").intValue();
+                Pair<Integer, double[]> neighbor_update = new Pair<>(-1, new double[0]);
+                try {
+                    Socket client = new Socket("localhost", port_origin);
+                    InputStream inputToServer = client.getInputStream();
+                    ObjectInputStream input = new ObjectInputStream(inputToServer);
+                    neighbor_update = (Pair<Integer, double[]>)input.readObject();
+                    client.close();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+//              todo: updates updates vector, matrix, broadcast to other neighbor what we got
+                int neighbor_id = neighbor_update.getKey();
+                if (neighbor_id == -1 || updated.get(neighbor_id))
+                    continue;
+                else {
+                    updated.put(neighbor_id, true);
+                    this.matrix[neighbor_id-1] = neighbor_update.getValue();
+                    for (int j = 0; j < neighbors.size(); j++){
+//                      we send to the other neighbors the lv from the one we just got
+                        if (i != j){
+                            Map<String, Double> neighbor_to_broadcast =  (Map<String, Double>)neighbors.get(i).getValue();
+                            send_data(neighbor_to_broadcast, neighbor_update);
+                        }
+                    }
+                }
+            }
+        }
+//        returns true because finished all updates
+//        return true;
+    }
+
+
+    public void send_data(Map<String, Double> neighbor, Pair<Integer, double[]> lv){
+        int port_dest = neighbor.get("port_dest").intValue();
+        try {
+            Socket client = new Socket("localhost", port_dest);
+            OutputStream outToServer = client.getOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(outToServer);
+            out.writeObject(lv);
+            client.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 //    todo: how should the printing graph look like? only relevant vector or all matrix?
